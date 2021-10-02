aee7563577cd5d602b58c428131c94163d260ed2
Walther Seidel <waseidel@gmail.com>
2021-10-01 19:51:43 -0500

    updated 211001 with tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("basic math testing", () => {
+  it("finds the sum of a + b", () => {
+    const a = 10;
+    const b = 30;
+    const sum = a + b;
+    expect(sum).toBe(40);
+  });
+
+  it("finds the times of a * b", () => {
+    const a = 10;
+    const b = 30;
+    const mult = a * b;
+    expect(mult).toBe(300);
+  });
 });
